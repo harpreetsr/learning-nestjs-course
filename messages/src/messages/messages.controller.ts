11b67aef947f9b7bebbd7f599243d789808f7da3
Harpreet S. Randhawa <harpreetwp@gmail.com>
2022-08-14 16:16:02 +0530

    Init commit

---
+++ messages/src/messages/messages.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message-dto';
+
+@Controller('messages')
+export class MessagesController {
+  @Get()
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  listMessages() {}
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    console.log(body);
+  }
+
+  @Get('/:id')
+  getMessage(@Param() id: string) {
+    console.log(id);
+  }
+}
