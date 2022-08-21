265b37134790540e6a6b349cf5bcb6411b680606
Harpreet S. Randhawa <harpreetwp@gmail.com>
2022-08-21 13:28:48 +0530

    NestArchitecture : Organizing code with Modules

---
+++ di/src/power/power.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class PowerService {
+  supplyPower(watts: number) {
+    console.log(`Supplying ${watts} of power!`);
+  }
+
+  data() {
+    console.log('Data!');
+  }
+}
