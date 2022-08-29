448cd04540adbc26ea51dc4ed9ba7d69ce46b88f
Harpreet S. Randhawa <harpreetwp@gmail.com>
2022-08-29 10:59:36 +0530

    Section 11: Setting up Sessions

--- carvalue/dist/main.js
+++ carvalue/dist/main.js
@@ -3,8 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const common_1 = require("@nestjs/common");
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const cookie_session_1 = require("cookie-session");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.use((0, cookie_session_1.default)({
+        keys: ['HSRANDHAWA'],
+    }));
     app.useGlobalPipes(new common_1.ValidationPipe({
         whitelist: true,
     }));
--- carvalue/src/main.ts
+++ carvalue/src/main.ts
@@ -1,9 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cookieSession from 'cookie-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    cookieSession({
+      keys: ['HSRANDHAWA'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
