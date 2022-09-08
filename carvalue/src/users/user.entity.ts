bfe3ef3e3bf8b22a40f961fbf52d1cc586615aea
Harpreet S. Randhawa <harpreetwp@gmail.com>
2022-09-08 19:00:07 +0530

    Section 15: The ManyToOne and OneToMany Decorators

--- carvalue/src/users/user.entity.ts
+++ carvalue/src/users/user.entity.ts
@@ -1,3 +1,4 @@
+import { Report } from 'src/reports/report.entity';
 import {
   AfterInsert,
   AfterRemove,
@@ -5,6 +6,7 @@ import {
   Entity,
   Column,
   PrimaryGeneratedColumn,
+  OneToMany,
 } from 'typeorm';
 @Entity()
 export class User {
@@ -16,6 +18,9 @@ export class User {
 
   @Column()
   password: string;
+
+  @OneToMany(() => Report, (report) => report.user)
+  reports: Report[];
 
   @AfterInsert()
   logInsert() {
